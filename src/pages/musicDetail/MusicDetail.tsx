b10ee1eb57d6895bfdeec440e5b3a8f946547d2e
yeooong-dev <yeooong12@naver.com>
2023-03-12 03:29:16 +0900

    feat/댓글 페이지 UI 완료

--- src/pages/musicDetail/MusicDetail.tsx
+++ src/pages/musicDetail/MusicDetail.tsx
@@ -1,15 +1,34 @@
-import React from "react";
-import Footer from "../../components/footer/Footer";
-import Header from "../../components/header/Header";
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
+import {
+  Info,
+  ComposerImg,
+  ComposerName,
+  MusicDesc,
+  PlayBtn,
+  CommentBox,
+  CommentInput,
+  Wrap,
+  CommentBtn,
+} from './MusicDetailSt'
 
 function MusicDetail() {
   return (
-    <>
+    <Wrap>
       <Header />
-      <div>MusicDetail</div>
+      <Info>
+        <ComposerImg></ComposerImg>
+        <ComposerName>작곡가 이름</ComposerName>
+        <MusicDesc>곡 설명입니다~~~~~~~~</MusicDesc>
+        <PlayBtn>재생버튼</PlayBtn>
+      </Info>
+      <CommentBox>
+        <CommentInput></CommentInput>
+        <CommentBtn>댓글 작성</CommentBtn>
+      </CommentBox>
       <Footer />
-    </>
-  );
+    </Wrap>
+  )
 }
 
-export default MusicDetail;
+export default MusicDetail
