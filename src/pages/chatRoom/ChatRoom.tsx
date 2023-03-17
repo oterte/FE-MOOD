0d1bccf2e5b2ebff3dfed32c80fffd33172d9612
jaeuk Lee <lskkhj1005@naver.com>
2023-03-17 20:14:59 +0900

    feat: scroll to bottom

--- src/pages/chatRoom/ChatRoom.tsx
+++ src/pages/chatRoom/ChatRoom.tsx
@@ -1,118 +1,139 @@
-import React from "react";
-import { useState, useEffect, useCallback } from "react";
-import { useParams } from "react-router-dom";
-import { io } from "socket.io-client";
+import React, { useRef } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import { useParams } from 'react-router-dom'
+import { io } from 'socket.io-client'
+import {
+  StDivChatRoomChatListContain,
+  StDivChatRoomChatListWrap,
+  StDivChatRoomWrap,
+  StPChatRoom,
+} from './ChatRoomSt'
 
 interface ChatData {
-  param?: string;
-  message: string;
-  nickname: string;
+  param?: string
+  message: string
+  nickname: string
 }
 interface RecieveData {
-  message: string | null;
-  nickname: string | null;
+  message: string | null
+  nickname: string | null
 }
 interface BeforeChatData {
-  chatId: number;
-  roomId: number;
-  nickname: string;
-  message: string;
-  createdAt: string;
-  updatedAt: string;
+  chatId: number
+  roomId: number
+  nickname: string
+  message: string
+  createdAt: string
+  updatedAt: string
 }
 
-const socket = io(`http://54.180.96.111:3000`);
+const socket = io(`${process.env.REACT_APP_SERVER}`)
 const initSocketConnection = () => {
-  socket.connect();
-};
+  socket.connect()
+}
 const disconnection = () => {
-  socket.disconnect();
-};
+  socket.disconnect()
+}
 
 function FirstTest() {
-  const [chatText, setChatText] = useState<string>("");
-  const [recieveData, setRecieveData] = useState<RecieveData[]>([]);
-  const [beforeChatData, setBeforeChatData] = useState<BeforeChatData[]>([]);
-  const [userList, setUserList] = useState<string[]>([]);
+  const [chatText, setChatText] = useState<string>('')
+  const [recieveData, setRecieveData] = useState<RecieveData[]>([])
+  const [beforeChatData, setBeforeChatData] = useState<BeforeChatData[]>([])
+  const [userList, setUserList] = useState<string[]>([])
 
-  const param = useParams();
+  const param = useParams()
 
-  const nickname: string = "jaeuk";
-  const roomId: number = Number(param.id);
+  const scrollRef = useRef<HTMLDivElement>(null)
+
+  const scrollToBottom = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+    }
+  }
+
+  const nickname: string = 'jaeuk'
+  const roomId: number = Number(param.id)
   // 들어왔을 때 socket을 연결하고 unmount 되면 socket을 disconnect
   useEffect(() => {
-    initSocketConnection();
-    socket.emit("roomId", roomId);
-    socket.emit("newUser", nickname);
+    initSocketConnection()
+    socket.emit('roomId', roomId)
+    socket.emit('newUser', nickname)
     return () => {
-      socket.emit("offUser", nickname);
-      setUserList(userList.filter((userList) => userList !== nickname));
-      disconnection();
-    };
-  }, []);
+      socket.emit('offUser', nickname)
+      setUserList(userList.filter((userList) => userList !== nickname))
+      disconnection()
+    }
+  }, [])
 
   const chatData: ChatData = {
     message: chatText,
     nickname: nickname,
-  };
+  }
   const onSubmitChattingHandler = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
+    e.preventDefault()
+  }
   const onChangeChatTextHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setChatText(e.target.value);
-  };
+    setChatText(e.target.value)
+  }
 
   const onClickSendMessageHandler = useCallback(() => {
-    const noContent = chatText.trim() === "";
+    const noContent = chatText.trim() === ''
     if (noContent) {
-      return;
+      return
     } else {
-      socket.emit("sendMessage", chatData);
-      setChatText("");
+      socket.emit('sendMessage', chatData)
+      setChatText('')
     }
-  }, [chatData]);
+  }, [chatData])
 
   useEffect(() => {
-    socket.on("onUser", (data) => {
-      setUserList([...userList, data]);
-    });
-  }, [userList]);
+    scrollToBottom()
+  }, [chatData, recieveData])
 
   useEffect(() => {
-    socket.on("receive", (data) => {
-      setBeforeChatData(data);
-    });
-  }, [beforeChatData]);
+    socket.on('onUser', (data) => {
+      setUserList([...userList, data])
+    })
+  }, [userList])
 
   useEffect(() => {
-    socket.on("receiveMessage", (data) => {
-      console.log("receiveMessage", data);
-      setRecieveData([...recieveData, data]);
-    });
-  }, [recieveData]);
+    socket.on('receive', (data) => {
+      setBeforeChatData(data)
+    })
+  }, [beforeChatData])
 
-  console.log(userList);
+  useEffect(() => {
+    socket.on('receiveMessage', (data) => {
+      console.log('receiveMessage', data)
+      setRecieveData([...recieveData, data])
+    })
+  }, [recieveData])
 
   return (
-    <div style={{ margin: "30px" }}>
-      {beforeChatData?.map((beforeChatData) => {
-        return (
-          <div key={beforeChatData.chatId}>
-            <p>
-              {beforeChatData.nickname} : {beforeChatData.message}
-            </p>
-          </div>
-        );
-      })}
-      {recieveData.map((recieveData, index) => {
-        return (
-          <div key={`${recieveData.message} + ${index}`}>
-            <p>
-              {recieveData.nickname} : {recieveData.message}
-            </p>
-          </div>
-        );
-      })}
+    <StDivChatRoomWrap>
+      <StDivChatRoomChatListWrap ref={scrollRef}>
+        {beforeChatData?.map((beforeChatData) => {
+          return (
+            <StDivChatRoomChatListContain key={beforeChatData.chatId}>
+              <StPChatRoom>
+                {beforeChatData.nickname} : {beforeChatData.message}
+              </StPChatRoom>
+            </StDivChatRoomChatListContain>
+          )
+        })}
+
+        {recieveData.map((recieveData, index) => {
+          return (
+            <StDivChatRoomChatListContain
+              key={`${recieveData.message} + ${index}`}
+            >
+              <StPChatRoom>
+                {recieveData.nickname} : {recieveData.message}
+              </StPChatRoom>
+            </StDivChatRoomChatListContain>
+          )
+        })}
+      </StDivChatRoomChatListWrap>
 
       <div>
         <form onSubmit={onSubmitChattingHandler}>
@@ -128,14 +149,14 @@ function FirstTest() {
         {userList.map((v) => {
           return (
             <div key={v}>
-              {" "}
-              <p>{v}</p>{" "}
+              {' '}
+              <p>{v}</p>{' '}
             </div>
-          );
+          )
         })}
       </div>
-    </div>
-  );
+    </StDivChatRoomWrap>
+  )
 }
 
-export default FirstTest;
+export default FirstTest
