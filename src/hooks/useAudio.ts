78cd75df9426fa8214e1841e6a96125cfa185de4
jaeuk Lee <lskkhj1005@naver.com>
2023-03-25 19:11:39 +0900

    #36 feat: api, musicId state

--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -1,20 +1,26 @@
-import { useRef, useState } from 'react'
+import { Dispatch, SetStateAction, useRef, useState } from 'react'
+import { postStreaming } from '../api/streamingApi'
 
-export type UseAudioReturnType = [() => void, React.RefObject<HTMLAudioElement>]
+export type UseAudioReturnType = [
+  () => void,
+  React.RefObject<HTMLAudioElement>,
+  Dispatch<SetStateAction<number>>
+]
 
 function useAudio(): UseAudioReturnType {
   const audioRef = useRef<HTMLAudioElement | null>(null)
   const [timer, setTimer] = useState<number>(0)
+  const [musicId, setMusicId] = useState(0)
 
   const handleTimeUpdate = () => {
     if (audioRef.current?.currentTime && audioRef.current.currentTime >= 3) {
       setTimer((prev) => prev + 1)
     }
   }
-  if (timer === 10) {
-    console.log('api 작동')
+  if (timer === 30) {
+    postStreaming(musicId)
   }
-  return [handleTimeUpdate, audioRef]
+  return [handleTimeUpdate, audioRef, setMusicId]
 }
 
 export default useAudio
