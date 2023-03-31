d65200642867fe9b704af31c85677704e1cc9ab4
oterte <97093479+oterte@users.noreply.github.com>
2023-03-31 22:27:56 +0900

    style: 마이페이지 2차

--- src/pages/mypage/MyPageTable.ts
+++ src/pages/mypage/MyPageTable.ts
@@ -1,20 +1,102 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const MyPageTableContainer = styled.table`
-    width: 100%;
-    border: 1px solid #d9d9d9;
-    text-align: center;
-    
+export const MyPageBodyTop = styled.div`
+  width: 1259px;
+  height: 70px;
+  background-color: #8b7d76;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
-export const MyPageTableThead = styled.thead`
-    background-color: #8b7d76;
-    color: black;
+export const MyPageBodyDiv = styled.div`
+  width: 33%;
+  font-style: normal;
+  font-size: 20px;
+  font-weight: 400;
+  font-family: var(--font-NotoSansKR-Regular);
+  line-height: 29px;
+  text-align: center;
+  color: #000000;
 `
-export const MyPageTableContentTd = styled.td`
-    font-style: normal;
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 23x;
-    color: #000000;
-    border: 1px solid #d9d9d9;
+export const MyPageBodyMiddle = styled.div`
+  width: 1259px;
+  height: 639px;
+  background-color: #fefefe;
+  border: 1px solid #d9d9d9;
+  display: flex;
+  flex-direction: column;
+`
+export const MyPageBodyMiddleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+export const MyPageBodyMiddleDiv = styled.div`
+  width: 33%;
+  font-style: normal;
+  font-size: 20px;
+  font-weight: 400;
+  font-family: var(--font-NotoSansKR-Regular);
+  line-height: 23px;
+  text-align: center;
+  color: #000000;
+  border-bottom: 1px solid #d9d9d9;
+`
+export const MyPageMiddleDivCursor = styled.div`
+  width: 33%;
+  font-style: normal;
+  font-size: 20px;
+  font-weight: 400;
+  font-family: var(--font-NotoSansKR-Regular);
+  line-height: 23px;
+  text-align: center;
+  color: #000000;
+  border-bottom: 1px solid #d9d9d9;
+  cursor: pointer;
+`
+export const MyPageLikeTopFirst = styled.div`
+  width: 25%;
+  font-style: normal;
+  font-size: 20px;
+  font-weight: 400;
+  font-family: var(--font-NotoSansKR-Regular);
+  line-height: 29px;
+  text-align: center;
+  color: #000000;
+`
+export const MyPageLikeTopSec = styled.div`
+  width: 25%;
+  font-style: normal;
+  font-size: 20px;
+  font-weight: 400;
+  font-family: var(--font-NotoSansKR-Regular);
+  line-height: 29px;
+  text-align: center;
+  color: #000000;
+`
+export const MyPageLikeTopRest = styled.div`
+  width: 25%;
+  font-style: normal;
+  font-size: 20px;
+  font-weight: 400;
+  font-family: var(--font-NotoSansKR-Regular);
+  line-height: 29px;
+  text-align: center;
+  color: #000000;
+`
+
+export const MyPageLikeMiddleContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+export const MyPageLikeMiddleDiv = styled.div`
+  width: 25%;
+  font-style: normal;
+  font-size: 20px;
+  font-weight: 400;
+  font-family: var(--font-NotoSansKR-Regular);
+  line-height: 23px;
+  text-align: center;
+  color: #000000;
+  border-bottom: 1px solid #d9d9d9;
 `
