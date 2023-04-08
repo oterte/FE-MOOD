f5ffa171f49511b6bb773671ac46fe7641976da0
oterte <97093479+oterte@users.noreply.github.com>
2023-04-08 22:35:18 +0900

    feat: 토큰 갱신 추가

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosInstance } from 'axios'
 import { onSetLocalStorageHandler } from '../util/cookie'
 
+
+
 const instance: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER,
   headers: { Authorization: localStorage.getItem('authorization') },
@@ -8,6 +10,10 @@ const instance: AxiosInstance = axios.create({
 })
 
 const withoutTokenInstance = axios.create({
+  baseURL: process.env.REACT_APP_SERVER,
+})
+
+const refreshInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER,
 })
 
@@ -19,27 +25,24 @@ instance.interceptors.request.use(
   },
   (error) => Promise.reject(error)
 )
-
+refreshInstance.interceptors.request.use((config) => {
+  const refresh = localStorage.getItem('refresh')
+  config.headers.Authorization = `Bearer ${refresh}`
+  return config
+},
+(error) => Promise.reject(error)
+)
 //Axios의 인터셉터를 사용하여,
 //419 응답을 받았을 때 refresh token을 사용하여
 //새로운 access token을 가져오도록 설정
 instance.interceptors.response.use(
   (response) => response,
   async (error) => {
-    const refreshToken = localStorage.getItem('refresh')
-    console.log("refreshToken", refreshToken)
-    const access = localStorage.getItem('authorization')
     const originalRequest = error.config
     if (error.response.status === 419) {
-      console.log('갱신 시도중')
       originalRequest._retry = true
-      const body = {
-        refreshToken,
-      }
       try {
-        console.log('토큰 갱신 api 실행')
-        const res = await instance.post(`/api/user/refresh`, refreshToken)
-        console.log(res)
+        const res = await refreshInstance.post(`/api/user/refresh`)
         const data = res.data
         onSetLocalStorageHandler('authorization', data.accessToken)
         onSetLocalStorageHandler('refresh', data.refreshToken)
