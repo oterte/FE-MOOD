b0cbbeb92eee8b76a10ab1dc90c33358b3046eed
jaeuk Lee <lskkhj1005@naver.com>
2023-04-08 12:39:39 +0900

    feat: google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,22 @@ import { QueryClient } from 'react-query'
 import Router from './shared/Router'
 import { Provider } from 'react-redux'
 import store from './redux/config/configStore'
+import ReactGA from 'react-ga'
+import { createBrowserHistory } from 'history'
 
 function App() {
   const queryClient = new QueryClient()
+
+  const gaTrackingId: any = process.env.REACT_APP_GA_TRACKING_ID
+  ReactGA.initialize(gaTrackingId, { debug: true })
+  ReactGA.pageview(window.location.pathname)
+
+  const history = createBrowserHistory()
+  history.listen((response) => {
+    console.log(response.location.pathname)
+    ReactGA.set({ page: response.location.pathname })
+    ReactGA.pageview(response.location.pathname)
+  })
 
   return (
     <QueryClientProvider client={queryClient}>
